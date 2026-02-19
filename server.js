d54f301a14bf44ff270b0479a61fe160ee6ab435
Nitish <nitishv712@gmail.com>
2026-02-19 23:29:39 +0530

    ty

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ const AuthRoutes = require('./routes/auth');
 const TreeRoutes = require('./routes/tree');
 
 const app = express();
-
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 app.use('/api/auth', AuthRoutes);
